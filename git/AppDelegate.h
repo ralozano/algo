854d6b9279ddc2cfd8ecba92c16f0a62b64ae07f
Rafael Lozano <rafael.lozano.espinsa@gmail.com>
2013-04-02 19:21:16 -0600

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by PROFESOR on 4/2/13.
+//  Copyright (c) 2013 PROFESOR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by PROFESOR on 4/2/13.
+//  Copyright (c) 2013 PROFESOR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
